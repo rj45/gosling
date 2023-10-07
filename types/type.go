a1ae031c6d0dbf2f166d87f3ec551f87ce41a9d8
rj45 <ryan@rj45.ca>
2023-10-07 10:15:07 -0700

    Reduce size of types.Type

--- types/type.go
+++ types/type.go
@@ -7,37 +7,43 @@ const (
 	FuncType
 )
 
+// Type identifies a type within the universe of types.
+//
+// It is 24 bits long:
+// 4 bits for the TypeKind
+// 2 bits for the number of pointer indirections
+// 18 bits for the index into the universe of types
 type Type uint32
 
 func newType(kind TypeKind, index int, indirections int) Type {
 	if kind < BasicType || kind > FuncType {
 		panic("kind out of range")
 	}
-	if index < 0 || index > 0xffffff {
-		panic("index must be between 0 and 0xffffff")
+	if index < 0 || index > 0x3ffff {
+		panic("index must be between 0 and 0x3ffff")
 	}
 	if indirections < 0 || indirections > 3 {
 		panic("indirections must be between 0 and 3")
 	}
 
-	return Type((uint32(kind) << 28) | (uint32(indirections&3) << 24) | uint32(index))
+	return Type((uint32(kind) << 20) | (uint32(indirections&3) << 18) | uint32(index))
 }
 
 // Kind returns the kind of type.
 func (t Type) Kind() TypeKind {
-	return TypeKind(t >> 28)
+	return TypeKind(t >> 20)
 }
 
 // Index into the universe of types.
 func (t Type) Index() int {
-	return int(t & 0xffffff)
+	return int(t & 0x3ffff)
 }
 
 // Indirections returns the number of pointer indirections.
 // That is, `p` is 0, `*p` is 1, `**p` is 2, etc.
 // The maximum number of indirections is 3.
 func (t Type) Indirections() int {
-	return int((t >> 24) & 3)
+	return int((t >> 18) & 3)
 }
 
 func (t Type) Deref() Type {
